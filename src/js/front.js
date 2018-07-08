10623fbcfe2f1f41f386ad7157646a1dc187b53a
nazariy17 <nazariy2410@gmail.com>
2018-07-08 21:38:53 +0300

    initial commit

---
+++ src/js/front.js
@@ -0,0 +1,127 @@
+let front = {
+
+    hamburger: $('.hamburger'),
+    nav: $('.header__list'),
+
+    slider_options_default: {
+        wrapAround: true,
+        pageDots: false,
+        prevNextButtons: true,
+        autoPlay: true,
+        cellAlign: 'left',
+        contain: true,
+        imagesLoaded: true
+    },
+
+    init: function () {
+        this.events();
+    },
+
+    newSlider: function (selector, options) {
+        options = (options !== undefined) ? Object.assign({}, this.slider_options_default, options) : this.slider_options_default;
+        // let carousel = new Flickity(document.querySelector(selector), options);
+        return new Flickity(document.querySelector(selector), options);
+    },
+
+    toogleNav: function(){
+        if (!this.hamburger.hasClass('is-active')) {
+            this.hamburger.addClass("is-active");
+            this.nav.slideToggle();
+        }
+        else {
+            this.hamburger.removeClass("is-active");
+            this.nav.slideToggle();
+        }
+    },
+
+    events: function () {
+        let self = this;
+
+        $(document).on('click', '.hamburger', function () {
+            self.toogleNav();
+        });
+
+        $('.js-scrollLink').on('click', function (e) {
+            e.preventDefault();
+            let target = this.hash;
+            let $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top -80
+            }, 500, 'swing');
+        });
+
+        $(document).on('change', '#file', function (event) {
+            let self = $(this);
+            let file = event.target.files[0];
+            let translate_txt = self.parent().find('label').attr('data-translate');
+
+            let fileExtension = ['jpeg', 'png', 'jpg'];
+
+            if ($.inArray(self.val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+                self.parent().find('label').text(translate_txt + fileExtension.join(', '));
+                self.val('');
+                return false;
+            } else {
+                self.parent().find('label').text(file.name);
+            }
+        });
+
+
+    }
+};
+
+
+
+let modal = {
+    closeButton: $('.modal__close'),
+    closeOverlay: $('.modal'),
+    can: 1,
+    init: function () {
+        this.events();
+    },
+    openModal: function (id) {
+        let modalWindow = $(id);
+        modalWindow.fadeIn();
+        modalWindow.find('.modal__content').removeClass('animate-away').addClass('animate-in');
+    },
+
+    closeModal: function (id) {
+        let modalWindow = $(id);
+        modalWindow.find('.modal__content').removeClass('animate-in').addClass('animate-away');
+        modalWindow.fadeOut();
+    },
+
+    events: function () {
+
+        $(document).on('click', '.modalTrigger', function (e) {
+            e.preventDefault();
+            let self = $(this),
+                target = self.attr('data-modal');
+            modal.openModal(target);
+
+        });
+
+        $(document).on('click', '.modal', function (event) {
+            let self = '#' + $(this).attr('id');
+            if (event.target.className == 'modal__body') {
+                modal.closeModal(self);
+            }
+        });
+
+        $(document).on('click', '.modal__close', function () {
+            let self = '#' + $(this).closest('.modal').attr('id');
+            modal.closeModal(self);
+        });
+
+
+
+
+    }
+};
+
+
+
+jQuery(function () {
+    front.init();
+    modal.init();
+});
